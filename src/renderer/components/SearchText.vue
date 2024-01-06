c46d5cc8e7560da279d3925ca2ae37ce87c76824
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-01-06 17:52:50 +0200

    keyboard short cuts, only search yet

--- src/renderer/components/SearchText.vue
+++ src/renderer/components/SearchText.vue
@@ -1,12 +1,26 @@
 <script setup>
+import { ref } from 'vue';
+import Mousetrap from 'mousetrap';
 import { useClipboardStore } from '../stores/useClipboardStore';
+import keyboard from '../keyshortcuts';
 
 const clipboardStore = useClipboardStore()
+const searchInput = ref(null)
+
+const focus = () => {
+    if (searchInput.value) {
+        searchInput.value.focus()
+    }
+}
+
+Mousetrap.bind(keyboard.search, focus);
 </script>
 
 <template>
     <input type="text"
         class="w-full rounded-lg  bg-slate-100 focus:bg-slate-50 p-1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-300"
         v-model="clipboardStore.filter"
-        placeholder="Type here to search..." />
+        ref="searchInput"
+        placeholder="Type here to search..."
+    />
 </template>
