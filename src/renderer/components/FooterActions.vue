820a637d3dfd9b60217959a4fc896d7bf5e19312
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-02-03 13:03:09 +0200

    image filter

--- src/renderer/components/FooterActions.vue
+++ src/renderer/components/FooterActions.vue
@@ -5,16 +5,23 @@ import ToolButton from './ToolButton.vue';
 import IconTrash from './icons/IconTrash.vue';
 import IconStarOutline from './icons/IconStarOutline.vue';
 import IconStarSolid from './icons/IconStarSolid.vue';
+import IconImageOutline from './icons/IconImageOutline.vue';
+import IconImageSolid from './icons/IconImageSolid.vue';
 import { useClipboardStore } from '../stores/useClipboardStore';
 import { keyboard } from '../keyshortcuts';
 
 const clipboardStore = useClipboardStore()
 
 const hasClips = computed(() => clipboardStore.clips.length > 0)
+const hasImages = computed(() => clipboardStore.images.length > 0)
 
 const showStarred = ref(false)
+const showImages = ref(false)
 
-onMounted(() => Mousetrap.bind(keyboard.toggleFavorites, toggleStarred))
+onMounted(() => {
+    Mousetrap.bind(keyboard.toggleFavorites, toggleStarred)
+    Mousetrap.bind(keyboard.toggleImages, toggleImages)
+})
 
 function clear() {
     if (!hasClips.value) {
@@ -23,10 +30,16 @@ function clear() {
     
     let msg
     let clearCallback
-    
-    if (showStarred.value) {
-        msg = 'Are you sure you want to remove all starred items ?'
+
+    if (showStarred.value && showImages.value) {
+        msg = 'Are you sure you want to remove starred images ?'
+        clearCallback = (item) => item.starred && item.image
+    } else if (showStarred.value) {
+        msg = 'Are you sure you want to remove starred items ?'
         clearCallback = (item) => item.starred
+    } else if (showImages.value) {
+        msg = 'Are you sure you want to remove images ?'
+        clearCallback = (item) => item.image && !item.starred
     } else {
         msg = 'Are you sure you want to remove items ?'
         clearCallback = (item) => ! item.starred
@@ -45,6 +58,11 @@ function toggleStarred() {
     showStarred.value = !showStarred.value
     clipboardStore.onlyStarred = showStarred.value
 }
+
+function toggleImages() {
+    showImages.value = !showImages.value
+    clipboardStore.withImages = showImages.value
+}
 </script>
 
 <template>
@@ -52,6 +70,10 @@ function toggleStarred() {
         <ToolButton class="w-8 h-8" @click="toggleStarred">
             <IconStarSolid v-if="showStarred" />
             <IconStarOutline v-else />
+        </ToolButton>
+        <ToolButton class="w-8 h-8" v-if="hasImages" @click="toggleImages">
+            <IconImageSolid v-if="showImages" />
+            <IconImageOutline v-else />
         </ToolButton>
         <ToolButton class="w-8 h-8" @click="clear" :disabled="!hasClips">
             <IconTrash />
--- src/renderer/keyshortcuts.js
+++ src/renderer/keyshortcuts.js
@@ -1,6 +1,7 @@
 const keyboard = {
     "search": "ctrl+k",
     "toggleFavorites": "ctrl+/",
+    "toggleImages": "ctrl+i",
 }
 
 export { keyboard }
