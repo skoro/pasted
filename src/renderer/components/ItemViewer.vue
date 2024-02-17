38f78fd288beb36909341a4005c6885f829dbe4f
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-02-17 15:11:58 +0200

    copy & remove actions on viewer page

--- src/renderer/components/ItemViewer.vue
+++ src/renderer/components/ItemViewer.vue
@@ -4,6 +4,8 @@ import ToolButton from './ToolButton.vue'
 import IconCopy from './icons/IconCopy.vue'
 import IconTrash from './icons/IconTrash.vue'
 import IconCross from './icons/IconCross.vue'
+import { useClipboardStore } from '../stores/useClipboardStore'
+import { toRaw } from 'vue'
 
 const props = defineProps({
     clip: {
@@ -11,6 +13,27 @@ const props = defineProps({
         required: true,
     }
 })
+
+const emit = defineEmits(['close-page'])
+
+const clipboard = useClipboardStore()
+
+function copyClip() {
+    window.electronAPI.selectClipModel(toRaw(props.clip))
+    closePage()
+}
+
+function removeClip() {
+    if (confirm('Are you sure you want to remove ?')) {
+        clipboard.remove(props.clip.id)
+        window.electronAPI.removeClipModel(props.clip.id)
+        closePage()
+    }
+}
+
+function closePage() {
+    emit('close-page')
+}
 </script>
 
 <template>
@@ -18,14 +41,14 @@ const props = defineProps({
 
         <HeaderBar class="text-gray-600 justify-between px-4">
             <div class="flex space-x-4 items-center">
-                <ToolButton class="h-8 w-8">
+                <ToolButton class="h-8 w-8" @click="copyClip">
                     <IconCopy></IconCopy>
                 </ToolButton>
-                <ToolButton class="h-8 w-8">
+                <ToolButton class="h-8 w-8" @click="removeClip">
                     <IconTrash></IconTrash>
                 </ToolButton>
             </div>
-            <ToolButton class="h-8 w-8" @click="$emit('close-page')">
+            <ToolButton class="h-8 w-8" @click="closePage">
                 <IconCross></IconCross>
             </ToolButton>
         </HeaderBar>
