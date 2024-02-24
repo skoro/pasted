fe2bacdb2ff52d97abcf54ba5a6f8e2b64e622ef
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-02-24 16:05:12 +0200

    ignore empty strings option

--- src/renderer/components/PagePreferences.vue
+++ src/renderer/components/PagePreferences.vue
@@ -2,10 +2,13 @@
 import HeaderBar from './HeaderBar.vue'
 import ToolButton from './ToolButton.vue'
 import IconCross from './icons/IconCross.vue'
+import { usePreferencesStore } from '../stores/usePreferencesStore'
 import Mousetrap from 'mousetrap'
 import { onMounted } from 'vue'
 
 const emit = defineEmits(['close-page'])
+
+const prefs = usePreferencesStore()
 
 onMounted(() => {
     Mousetrap.bind('esc', closePage)
@@ -24,5 +27,20 @@ function closePage() {
                 <IconCross></IconCross>
             </ToolButton>
         </HeaderBar>
+
+        <div class="my-11 p-4">
+            <div class="flex items-center mb-4">
+                <input
+                    id="opt-1"
+                    type="checkbox"
+                    class="w-4 h-4"
+                    v-model="prefs.ignoreEmptyStrings"
+                />
+                <label
+                    for="opt-1"
+                    class="text-sm ml-3 font-medium text-gray-900"
+                >Ignore empty string</label>
+            </div>
+        </div>
     </div>
 </template>
