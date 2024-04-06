44d6f67563d32b0528f13eb50e0774348d61c8d1
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-04-06 17:18:05 +0300

    only ubuntu supported

--- forge.config.js
+++ forge.config.js
@@ -20,10 +20,10 @@ module.exports = {
         },
       },
     },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    // {
+    //   name: '@electron-forge/maker-rpm',
+    //   config: {},
+    // },
   ],
   plugins: [
     {
